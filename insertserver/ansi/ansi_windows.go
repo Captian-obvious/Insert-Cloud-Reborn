a481f401f794d475838c917e3e111d3b3441cd8a
Superduperdev2 <84868516+Captian-obvious@users.noreply.github.com>
2026-07-05 13:30:09 -0400

    fix ansi package lol

--- insertserver/ansi/ansi_windows.go
+++ insertserver/ansi/ansi_windows.go
@@ -13,16 +13,16 @@ import (
 )
 
 const (
-	Reset        = "\x1b[0m"
-	Black        = "\x1b[30m"
-	Red          = "\x1b[31m"
-	Green        = "\x1b[32m"
-	Yellow       = "\x1b[33m"
-	Blue         = "\x1b[34m"
-	Magenta      = "\x1b[35m"
-	Cyan         = "\x1b[36m"
-	White        = "\x1b[37m"
-	DefaultColor = "\x1b[39m"
+	Reset        string = "\x1b[0m"
+	Black        string = "\x1b[30m"
+	Red          string = "\x1b[31m"
+	Green        string = "\x1b[32m"
+	Yellow       string = "\x1b[33m"
+	Blue         string = "\x1b[34m"
+	Magenta      string = "\x1b[35m"
+	Cyan         string = "\x1b[36m"
+	White        string = "\x1b[37m"
+	DefaultColor string = "\x1b[39m"
 )
 
 func EnableANSI() {
